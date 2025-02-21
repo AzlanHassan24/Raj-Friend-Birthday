e096f9379c5dd3bbfc15de13c19cbde7b0ab3f19
AzlanHassan24 <s.masoodukpk@gmail.com>
2025-02-21 17:22:11 +0000

    Add files via upload

---
+++ L2Program.py
@@ -0,0 +1,21 @@
+print("Hello my name is Raj and here are my friends' birthdays! ")
+
+print("Hello I'm Jaquavas and my birthday is the 23rd of january")
+
+print("Hello My name is Jamal and my Birthday is the 22nd of October.")
+
+print("Hello my name is Tyron and my birthday is on the 12th of november")
+
+print("What's up! My name is Riley and My Birthday is the 14th of March!")
+
+print("Hi! My name is Darulo and my birthday is on the 1st of april!")
+
+
+
+
+
+
+
+
+
+
